5c82a90cabae8cdf1c83d1b2b8db2f4d77bffde0
= <=>
2022-09-26 16:45:24 +0800

    upload

---
+++ upload/tambah.php
@@ -0,0 +1,60 @@
+<?php
+require 'functions.php';
+if (isset($_POST["submit"])) {
+    //cek apakah data berhasil ditambahkan atau tidak
+    if (tambah($_POST) > 0) {
+        echo "<script>
+        alert('Data berhasil ditambahkan')
+        document.location.href = 'index.php';
+        </script>";
+    } else {
+        echo "<script>
+        alert('Data gagal ditambahkan')
+        document.location.href = 'index.php';
+        </script>";
+    }
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tambah Data Mahasiswa</title>
+</head>
+
+<body>
+    <form action="" method="post" enctype="multipart/form-data">
+        <ul>
+            <li>
+                <label for="nrp">NRP:</label>
+                <input type="text" name="nrp" id="nrp" required>
+            </li>
+            <li>
+                <label for="nama">NAMA:</label>
+                <input type="text" name="nama" id="nama">
+            </li>
+            <li>
+                <label for="email">EMAIL:</label>
+                <input type="text" name="email" id="email">
+            </li>
+            <li>
+                <label for="jurusan">JURUSAN:</label>
+                <input type="text" name="jurusan" id="jurusan">
+            </li>
+            <li>
+                <label for="gambar">GAMBAR:</label>
+                <input type="file" name="gambar" id="gambar">
+            </li>
+            <li>
+                <button type="submit" name="submit">Tambah Data</button>
+            </li>
+        </ul>
+    </form>
+</body>
+
+</html>
